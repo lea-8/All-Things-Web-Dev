e12f8732effa1f2554b1a9f27fdbd90df1a41d9b
Lea <s1953043@ed.ac.uk>
2025-06-06 23:40:59 +0100

    Start react, tiny Svelte.

--- 3-Svelte/6-reactive-declarative/src/routes/App.svelte
+++ 3-Svelte/6-reactive-declarative/src/routes/App.svelte
@@ -1,19 +1,38 @@
 <script>
 	let count = 1;
   // let double = count * 2;
-  $: double = count *2;
+  $: console.log('double - before re-assignment:', double);
+  $: double = count * 2;
+  
+  // s. ab. Svelte will re-order stuff to make sure it's up-to-date
+  $: quadruple = double * 2;  // quadruple -> double
+  
+  $: console.log('count', count );
+  // double with no lag.
+  $: console.log('double - reactive no lag:', double);
+  $: console.log('double 2:', double);
+
+  // $: doSomething(a, b);  // depends on: doSomething, a, b
+  // function doSomething() {
+  //   console.log(a, b, count);
+  // }
+  // --- ^ ---
+  // s.ab. just because count changes, does not mean doSomething will be invoked reactively.
 	
 	function increment() {
 		count ++;
     // double = count * 2;
+
+    // value lags behind, but don't get the explanation.
+    console.log('double with lag:', double);
 	}
 	function decrement() {
 		count --;
-    // double = count * 2;
+    double = count * 2;
 	}
   function multiply() {
     count = count * count;
-    // double = count *2;
+    // double = count * 2;
   }
 
   function doSomething() {
@@ -22,7 +41,7 @@
 </script>
 
 <button on:click={decrement}>-</button>
-{count} * 2 = {double}
+{count} * 2 = {double} * 2 = {quadruple}
 <button on:click={increment}>+</button>
 <button on:click={multiply}>scale</button>
 
---
+++ 5-react/tutorial/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function Greeting() {
+    return <h2>My first component</h2>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
