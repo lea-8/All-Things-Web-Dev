1fc4e8b3f927b5e575841727f2136db70f6f6661
Lea <s1953043@ed.ac.uk>
2025-06-29 22:39:18 +0100

    Start binding:group

---
+++ 3-Svelte/13-bind-group/src/routes/App.svelte
@@ -0,0 +1,38 @@
+<script>
+  let option;
+  // let extra = "";
+
+  // $: if (option === "2"){
+  //   extra = ":)";
+  // }
+</script>
+
+<label>
+	<input type="radio" name="foo" bind:group={option} value="1"> 1
+</label>
+<label>
+	<input type="radio" name="foo" bind:group={option} value="2"> 2
+</label>
+<label>
+	<input type="radio" name="foo" bind:group={option} value="3"> 3
+</label>
+<label>
+	<input type="radio" name="foo" bind:group={option} value="4"> 4
+</label>
+
+<!-- Flavours: {flavours.join(', ') || '-'} -->
+Selected option: {option}
+<!-- {extra} -->
+
+<!-- <label>
+	<input type="radio" name="foo" bind:group={option} value="1"> 1
+</label>
+<label>
+	<input type="radio" name="foo" bind:group={option} value="2"> 2
+</label>
+<label>
+	<input type="radio" name="foo" bind:group={option} value="3"> 3
+</label>
+<label>
+	<input type="radio" name="foo" bind:group={option} value="4"> 4
+</label> -->
