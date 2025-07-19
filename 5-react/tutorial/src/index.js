f50678be8f3130c86ccbcff92d8e88077f7efb00
Lea <s1953043@ed.ac.uk>
2025-07-19 22:26:41 +0100

    Start Amazon clone

--- 5-react/tutorial/src/index.js
+++ 5-react/tutorial/src/index.js
@@ -1,54 +1,15 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 
-// function Greeting() {
-//   return (
-//     <div>
-//       <h2>My first component</h2>
-//     </div>
-//   );
-// }
-
-// function Greeting() {
-//   return React.createElement(
-//      'div',
-//       {},
-//       React.createElement('h2', {}, 'hello React')
-//   )
-// }
-
-// function Greeting() {
-//   return (
-//     // <div>
-//     <React.Fragment>
-//       <div className='thisIsActuallyAClass'>
-//         <h3>Hi</h3>
-//         <ul>
-//           <li>
-//             <a href="#">link</a>
-//           </li>
-//         </ul>
-//       </div>
-//       <h2>problems?</h2>
-//       <input type='text' name='' id='' />
-//     </React.Fragment>  // </div>
-//   );
-// }
-
-function Greeting() {
-  return (
-    <div>
-      <Person />
-      <Message />
-    </div>
-  );
+const BookList = () => {
+  <section></section>
 }
 
-const Person = () => <h2>John Oed</h2>
-const Message = () => {
-  return <p>Message text!</p>
+const Book = () => {
+  return <article></article>
 }
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const Image = () => <h2>Image placeholder</h2>
 
-root.render(<Greeting />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<BookList />);
