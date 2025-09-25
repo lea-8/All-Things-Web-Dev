da6853b384f415968ce0e67219cc0479c26a415a
Lea <s1953043@ed.ac.uk>
2025-09-25 14:27:52 +0200

    Plane org of '3-Svelte- dir and finish margin convention

--- 2-d3/13-bar-chart/src/index.js
+++ 2-d3/13-bar-chart/src/index.js
@@ -6,8 +6,9 @@ import { csv, arc, scaleBand, scaleLinear, max } from "d3";
 const csvUrl =
   "https://gist.githubusercontent.com/lea-8/cdbf7b36665e5f22452126cf1eaf2c49/raw/f4ee0dad1f05858d9c614017956723c69d975505/UN_Population_2023.csv";
 
-const width = 720;
-const height = 350;
+const width = 500;
+const height = 300;
+const margin = { top: 20, right: 20, bottom: 20, left:20 };
 
 const pieArc = arc().innerRadius(0).outerRadius(width);
 
@@ -30,24 +31,29 @@ const App = () => {
 
   console.log(data[0]);
 
+  const innerHeight = height - margin.top - margin.bottom;
+  const innerWidth = width - margin.left - margin.right;
+
   const yScale = scaleBand()
     .domain(data.map((d) => d.Country))
-    .range([0, height]);
+    .range([0, innerHeight]);
 
   const xScale = scaleLinear()
     .domain([0, max(data, (d) => d.Population)])
-    .range([0, width]);
+    .range([0, innerWidth]);
 
   return (
     <svg width={width} height={height}>
-      {data.map((d) => (
-        <rect 
-          x={0} 
-          y={yScale(d.Country)} 
-          width={xScale(d.Population)} 
-          height={yScale.bandwidth()}
-        />
-      ))}
+      <g transform={`translate(${margin.left}, ${margin.top})`}>
+        {data.map((d) => (
+          <rect 
+            x={0} 
+            y={yScale(d.Country)} 
+            width={xScale(d.Population)} 
+            height={yScale.bandwidth()}
+          />
+        ))}
+        </g>
     </svg>
   );
 };
