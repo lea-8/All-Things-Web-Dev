94568c817ce974f44f141ef8d18d737065d80cf1
Lea <s1953043@ed.ac.uk>
2025-10-04 21:37:10 +0200

    Continue with {#each} block

--- 3-Svelte/hello-svelte/src/routes/18-each-block/App.svelte
+++ 3-Svelte/hello-svelte/src/routes/18-each-block/App.svelte
@@ -1,9 +1,33 @@
 <script>
-  let colours = ['red', 'green', 'blue'];
+  let colours = [
+    { name: 'red', hex: '#f00' }, 
+    { name: 'green', hex: '#0f0' }, 
+    { name: 'blue', hex: '#00f' }, 
+    { name: 'yellow', hex: '#ff0' },
+    { name: 'black'}
+  ];
+
+  // colours.forEach
+  // for (let i = 0; i < colours.length; i++)
+
+  // ! The non-lazy way:
+  // for (const colour of colours) {
+  //   console.log(colour.nam, colour.hex);
+  // }
+
+  // ! The lazy way:
+  for (const { name, hex } of colours) {
+    console.log(name, hex);
+  }
 </script>
 
 <ul>
-  <li>{colours[0]}</li>
+  {#each colours as { name, hex }}
+    <!-- <li>{JSON.stringify(colour)}</li> -->
+    <li style="color: {hex}">{name}</li>
+  {/each}
+  <!-- <li>{colours[0]}</li>
   <li>{colours[1]}</li>
   <li>{colours[2]}</li>
+  <li>{colours[3]}</li> -->
 </ul>
