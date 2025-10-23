140997bde6f7ba9afc83b55a8a890a6e628eda03
Lea <s1953043@ed.ac.uk>
2025-10-23 16:34:32 +0100

    Add CSS to book list

--- 5-react/tutorial/src/index.js
+++ 5-react/tutorial/src/index.js
@@ -1,36 +1,45 @@
 import ReactDOM from "react-dom/client";
 
-import './index.css';
+import "./index.css";
 
 // Book list used: https://www.nytimes.com/books/best-sellers/
 
 const BookList = () => {
-  return <section className='booklist'>
-    <Book/>
-    <Book/>
-    <Book/>
-    <Book/>
-  </section>
-}
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+};
 
 const Book = () => {
-  return <article className='book'>
-    <Image/>
-    <Title/>
-    <Author/>
-  </article>
-}
+  return (
+    <article className="book">
+      {/* <Image />
+      <Title />
+      <Author /> */}
+      <img src="./images/book1.jpg" alt="Quicksilver (Deluxe Limited Edition)" />
+      <h2>Quicksilver (Deluxe Limited Edition)</h2>
+      <h4>Callie Hart</h4>
+    </article>
+  );
+};
 
-const Image = () => 
-<img 
-  src='https://static01.nyt.com/bestsellers/images/9781538774205.jpg'
-  alt='Quicksilver (Deluxe Limited Edition)'
-/>
-const Title = () => <h2>Quicksilver (Deluxe Limited Edition)</h2>
-const Author = () => {
-  return <h4>Callie Hart</h4>
-}
+// const Image = () => (
+// );
+// const Title = () => ;
+// const Author = () => {
+//   const inlineHeadingStyles = {
+//     color: "#617d98",
+//     fontSize: "0.75rem",
+//     marginTop: "0.5rem",
+//   };
+//   return
+// };
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<BookList />);
