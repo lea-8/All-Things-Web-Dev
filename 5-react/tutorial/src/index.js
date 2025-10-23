7c2f97712cd1f3668737d0e39778a866385e8fd1
Lea <s1953043@ed.ac.uk>
2025-10-23 21:08:13 +0100

    Use props to start rendering list more dynamically

--- 5-react/tutorial/src/index.js
+++ 5-react/tutorial/src/index.js
@@ -1,36 +1,31 @@
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
-const author = 'Callie Hart';
-const title = 'Quicksilver (Deluxe Limited Edition)';
-const img = './images/book1.jpg';
+
+const firstBook = {
+  author: 'Callie Hart',
+  title: 'Quicksilver (Deluxe Limited Edition)',
+  img: './images/book1.jpg'
+};
 
 // Book list used: https://www.nytimes.com/books/best-sellers/
 
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book author={firstBook.author} title={firstBook.title} img={firstBook.img} />
+      {/* <Book author={author} title={title} img={img} /> */}
     </section>
   );
 };
 
-const Book = () => {
+const Book = (props) => {
+  console.log(props);
   return (
     <article className='book'>
-      {/* <Image />
-      <Title />
-      <Author /> */}
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <h4>{author}</h4>
-      {/* Statement won 't cut it
-      <p>{let x = 6}</p>
-      But expresseion will
-      <p>{6 + 6}</p> */}
+      <img src={props.img} alt={props.title} />
+      <h2>{props.title}</h2>
+      <h4>{props.author}</h4>
     </article>
   );
 };
