10910a72dba7a7b201ae34b51165b03a6c940bf3
Lea <s1953043@ed.ac.uk>
2025-10-27 22:45:07 +0000

    Start using .map() to wrap elements in HTML
    T5-02:40

--- 5-react/tutorial/src/index.js
+++ 5-react/tutorial/src/index.js
@@ -2,27 +2,30 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
-const firstBook = {
-  author: 'Callie Hart',
-  title: 'Quicksilver (Deluxe Limited Edition)',
-  img: './images/book1.jpg'
-};
-const secondBook = {
-  author: 'Nicholas Sparks',
-  title: 'Remain',
-  img: 'https://static01.nyt.com/bestsellers/images/9798217154043.jpg'
-};
-
 // Book list used: https://www.nytimes.com/books/best-sellers/combined-print-and-e-book-fiction/
+
+const books = [
+  {
+    author: 'Callie Hart',
+    title: 'Quicksilver (Deluxe Limited Edition)',
+    img: './images/book1.jpg'
+  },
+  {
+    author: 'Nicholas Sparks',
+    title: 'Remain',
+    img: 'https://static01.nyt.com/bestsellers/images/9798217154043.jpg'
+  }
+];
+
+const names = ['john', 'peter', 'susan'];
+const newNames = names.map((name) => {
+  console.log(name);
+});
 
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book author={firstBook.author} title={firstBook.title} img={firstBook.img}>
-        {/* <p>Lorem ipsum dolor...</p>
-        <button>Click me</button> */}
-      </Book>
-      <Book author={secondBook.author} title={secondBook.title} img={secondBook.img} />
+      {names}
     </section>
   );
 };
