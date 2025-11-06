4291d964b2251a44b7ca12d990d25777ef1d6a32
Lea <s1953043@ed.ac.uk>
2025-11-06 18:53:37 +0000

    Finish NPM CLI basiscs

--- 6-node/3-npm/index.js
+++ 6-node/3-npm/index.js
@@ -1,3 +1,6 @@
 const { format } = require('date-fns');
+const { v4: uuid } = require('uuid');  // import v4 as uuid
 
-console.log(format(new Date(), 'yyyyMMdd\tHH:mm:ss'));
+console.log(format(new Date(), 'yyyy-MM-dd\tHH:mm:ss'));
+
+console.log(uuid());
